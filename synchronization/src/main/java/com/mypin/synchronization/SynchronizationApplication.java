95a48794c29d2efbcab8ae3fbd3213699ad647c6
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2024-02-01 23:10:06 +0100

    config #1: synchronization microservice

---
+++ synchronization/src/main/java/com/mypin/synchronization/SynchronizationApplication.java
@@ -0,0 +1,13 @@
+package com.mypin.synchronization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchronizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynchronizationApplication.class, args);
+	}
+
+}
