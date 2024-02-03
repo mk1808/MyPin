0f063bcfe5d3a854380bbeb61338e8cb2a26dd50
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2024-02-03 15:57:54 +0100

    config #3: create model and dto

---
+++ users/src/main/java/com/mypin/users/dtos/RegisterDto.java
@@ -0,0 +1,10 @@
+package com.mypin.users.dtos;
+
+public class RegisterDto {
+	
+	public String email;
+	public String login;
+	public String password;
+	public String repeatPassword;
+
+}
--- users/src/main/java/com/mypin/users/models/AppUser.java
+++ users/src/main/java/com/mypin/users/models/AppUser.java
@@ -21,6 +21,23 @@ public class AppUser {
 	private String email;
 	@NotBlank
 	private String login;
-
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
 
 }
