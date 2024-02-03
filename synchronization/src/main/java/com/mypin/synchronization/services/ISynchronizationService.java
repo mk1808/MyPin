8a5e2b05a87b28d7ba300b2c5dc5a536320aa73a
Marek Czopor <marqos111222@gmail.com>
2024-02-03 21:45:35 +0100

    config #3: synchronization - create controller methods

--- synchronization/src/main/java/com/mypin/synchronization/services/ISynchronizationService.java
+++ synchronization/src/main/java/com/mypin/synchronization/services/ISynchronizationService.java
@@ -1,8 +1,13 @@
 package com.mypin.synchronization.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.mypin.synchronization.dtos.SynchronizationDto;
 
 @Service
 public interface ISynchronizationService {
 
+	@PostMapping
+	public void sendSynchronizationMessage(SynchronizationDto synchronizationDto);
 }
--- synchronization/src/main/java/com/mypin/synchronization/services/SynchronizationService.java
+++ synchronization/src/main/java/com/mypin/synchronization/services/SynchronizationService.java
@@ -1,5 +1,13 @@
 package com.mypin.synchronization.services;
 
-public class SynchronizationService {
+import com.mypin.synchronization.dtos.SynchronizationDto;
+
+public class SynchronizationService implements ISynchronizationService{
+
+	@Override
+	public void sendSynchronizationMessage(SynchronizationDto synchronizationDto) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
