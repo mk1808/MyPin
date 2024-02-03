66d39fe87fa6cde19cdbe3e38efbca409b2c4764
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2024-02-03 15:56:40 +0100

    config #3: create model and connect db

---
+++ users/src/main/java/com/mypin/users/models/AppUser.java
@@ -0,0 +1,26 @@
+package com.mypin.users.models;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.UuidGenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class AppUser {
+	
+	@Id
+	@GeneratedValue
+	@UuidGenerator
+	private UUID id;
+	@Email
+	private String email;
+	@NotBlank
+	private String login;
+
+
+}
