91d41b48e206387b59ec9581cdb0458a6c164ffe
Marek Czopor <marqos111222@gmail.com>
2024-02-03 21:18:01 +0100

    config #3: notifications - create controller methods

---
+++ notifications/src/main/java/com/mypin/notifications/services/ICrudService.java
@@ -0,0 +1,14 @@
+package com.mypin.notifications.services;
+
+import java.util.UUID;
+
+public interface ICrudService<T> {
+	T save(T obj);
+
+	T update(T obj, UUID id);
+
+	T get(UUID id);
+
+	void delete(UUID id);
+
+}
