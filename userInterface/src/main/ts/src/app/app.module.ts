b8147fa060a782baa28aba2679bfbc4039114704
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2024-03-25 17:35:57 +0100

    feature #36: add taiga dependency

--- userInterface/src/main/ts/src/app/app.module.ts
+++ userInterface/src/main/ts/src/app/app.module.ts
@@ -1,8 +1,14 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER, TuiTextfieldControllerModule, TuiHintComponent, TuiHintModule, TuiButtonModule, TuiTooltipModule } from "@taiga-ui/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from "@angular/forms";
+import { TuiAccordionModule, TuiInputModule } from "@taiga-ui/kit";
+import { UNIVERSAL_PROVIDERS } from "@ng-web-apis/universal";
 
 @NgModule({
   declarations: [
@@ -10,10 +16,25 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    ReactiveFormsModule,
+    TuiInputModule,
+    TuiTextfieldControllerModule,
+    TuiHintModule,
+    TuiButtonModule,
+    TuiAccordionModule,
+    TuiTooltipModule,
+
   ],
   providers: [
-    provideClientHydration()
+    UNIVERSAL_PROVIDERS,
+    provideClientHydration(),
+    { provide: TUI_SANITIZER, useClass: NgDompurifySanitizer },
+
   ],
   bootstrap: [AppComponent]
 })
