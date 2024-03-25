26ccd4b51edafe9333342fef1104cad5120eb3d0
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2024-03-25 18:12:23 +0100

    feature #36: add animations dependency

--- userInterface/src/main/ts/src/app/app.component.ts
+++ userInterface/src/main/ts/src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { TuiDialogService } from '@taiga-ui/core';
+import { jelloAnimation } from 'angular-animations';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  animations: [jelloAnimation()]
 })
 export class AppComponent implements OnInit{
   title = 'my-pin';
+  animationState = false;
   readonly testForm = new FormGroup({
     testValue: new FormControl('mail@mail.ru'),
   });
@@ -21,14 +24,23 @@ export class AppComponent implements OnInit{
   ) { }
 
   ngOnInit(): void {
-     this.spinner.show();
+   /*  this.spinner.show();
 
     setTimeout(() => {
       this.spinner.hide();
-    }, 5000);
+    }, 1000);*/
+    this.animate()
   }
 
   open() {
     this.dialogs.open('Hello!').subscribe();
+  }
+
+  animate() {
+    this.animationState = false;
+    setTimeout(() => {
+      this.animationState = true;
+     
+    }, 5000);
   }
 }
